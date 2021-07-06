fe2c624c434cfb85362c0e10cedf53beaf8f8fdc
Vinicius Holtman <42218598+Vinicius-Holtman@users.noreply.github.com>
2021-07-06 17:15:10 -0300

    JobsCalc v1.1.0

--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
 const express = require("express")
+const Server = express()
 
-const server = express()
+server.use(express.static("public"))
 
-server.listen(2000, () => console.log('rodando'))
+Server.get('/', (req, res) => {
+    return res.sendFile(__dirname + "/views/index.html")
+})
+
+
+Server.listen(2000, () => console.log('rodando'))
